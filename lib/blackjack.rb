c6499d961c97f43da4a082caf8049857fc82b74e
Robert Defendini <robdefendini@gmail.com>
2020-06-06 22:11:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -85,6 +85,11 @@ end
 #####################################################
 
 def runner
-
-end
-binding.pry
+  welcome
+  hand=initial_round
+  until hand>21 do
+    hand=hit?(hand)
+    display_card_total(hand)
+  end
+  end_game(hand)
+end	
