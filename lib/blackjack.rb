0c772cb27726e6c7f57fef04893ea28a47d512f7
Robert Defendini <robdefendini@gmail.com>
2020-06-06 22:12:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -86,9 +86,9 @@ end
 
 def runner
   welcome
-  hand=initial_round
-  until hand>21 do
-    hand=hit?(hand)
+  hand = initial_round
+  until hand > 21 do
+    hand = hit?(hand)
     display_card_total(hand)
   end
   end_game(hand)
